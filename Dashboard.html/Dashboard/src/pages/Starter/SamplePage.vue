877fd65d94287ce2372ed487eef2d97e1143c234
DnG_Dherrick <dng_dherrick@DnG-Dherricks-MacBook-Air.local>
2019-10-29 08:12:07 +0800

    fetch metrics based on filters

--- Dashboard.html/Dashboard/src/pages/Starter/SamplePage.vue
+++ Dashboard.html/Dashboard/src/pages/Starter/SamplePage.vue
@@ -6,7 +6,7 @@
         <div class="row">
           <div class="col-lg-8 col-md-12 col-12">
             <div class="row">
-              <div class="col-lg-6 col-md-12 col-12">
+              <div class="col-lg-4 col-md-12 col-12">
                 <div class="d-flex">
                   <div class>
                     <label for>Choose Account</label>
@@ -30,7 +30,8 @@
                     v-model="accounts_select.multiple"
                     collapse-tags
                     filterable
-                    @blur="onSelectAccountOnFocus(true)"
+                    @change="checkIfAllAccountsSelected()"
+                    @visible-change="onSelectAccountOnFocus($event)"
                   >
                     <el-option
                       v-for="option in accounts_select.options"
@@ -43,7 +44,7 @@
                 </base-input>
               </div>
 
-              <div class="col-lg-6 col-md-12 col-12">
+              <div class="col-lg-8 col-md-12 col-12">
                 <div class="d-flex">
                   <div class>
                     <label for>Choose Campaign</label>
@@ -66,7 +67,7 @@
                     v-model="campaigns_select.multiple"
                     collapse-tags
                     filterable
-                    @focus="checkIfAllCampaignsSelected()"
+                    @change="checkIfAllCampaignsSelected()"
                   >
                     <el-option
                       v-for="option in campaigns_select.options"
@@ -80,19 +81,34 @@
               </div>
             </div>
           </div>
+
           <div class="col-lg-3 col-md-12 col-12">
             <div class="row">
               <div class="col-md-6 col-12">
                 <label for>Start Date</label>
                 <base-input>
-                  <el-date-picker type="date" placeholder="Select Date" v-model="start_date"></el-date-picker>
+                  <el-date-picker
+                    type="date"
+                    placeholder="Select Date"
+                    id="start_date"
+                    name="start_date"
+                    v-model="start_date"
+                    :picker-options="start_date_picker_options"
+                  ></el-date-picker>
                 </base-input>
               </div>
 
               <div class="col-md-6 col-12">
                 <label for>End Date</label>
                 <base-input>
-                  <el-date-picker type="date" placeholder="Selects Date" v-model="end_date"></el-date-picker>
+                  <el-date-picker
+                    type="date"
+                    placeholder="Select Date"
+                    id="end_date"
+                    name="end_date"
+                    v-model="end_date"
+                    :picker-options="end_date_picker_options"
+                  ></el-date-picker>
                 </base-input>
               </div>
             </div>
@@ -100,7 +116,11 @@
 
           <div class="col-lg-1 col-md-12 col-12">
             <label for></label>
-            <base-button type="primary" class="btn-block">
+            <base-button
+              type="primary"
+              class="btn-block"
+              @click.prevent="fetchAllAccountsAndCampaignsMetrics()"
+            >
               <i class="tim-icons icon-refresh-02"></i>
             </base-button>
           </div>
@@ -225,6 +245,57 @@ export default {
       },
       start_date: "",
       end_date: "",
+      start_date_picker_options: {
+        disabledDate(time) {
+          return time.getTime() > Date.now();
+        },
+        shortcuts: [
+          {
+            text: "Yesterday",
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24);
+              picker.$emit("pick", date);
+            }
+          },
+          {
+            text: "A week ago",
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit("pick", date);
+            }
+          },
+          {
+            text: "First day of the month",
+            onClick(picker) {
+              const date = new Date(),
+                y = date.getFullYear(),
+                m = date.getMonth();
+              var firstDay = new Date(y, m, 1);
+              date.setTime(firstDay);
+              picker.$emit("pick", date);
+            }
+          }
+        ]
+      },
+      end_date_picker_options: {
+        disabledDate(time) {
+          return time.getTime() > Date.now();
+        },
+        shortcuts: [
+          {
+            text: "Today",
+            onClick(picker) {
+              picker.$emit("pick", new Date());
+            }
+          }
+        ]
+      },
+      datePickerOptions: {
+        minimum_date: "",
+        maximum_date: ""
+      },
       accounts_select: {
         simple: "",
         multiple: "ARS",
@@ -234,6 +305,20 @@ export default {
         simple: "",
         multiple: "ARS",
         options: []
+      },
+      modelValidations: {
+        accounts_select: {
+          required: true
+        },
+        campaigns_select: {
+          required: true
+        },
+        start_date: {
+          required: true
+        },
+        end_date: {
+          required: true
+        }
       }
     };
   },
@@ -282,6 +367,9 @@ export default {
             });
           });
 
+          accounts.sort((a, b) => (a.color > b.color ? 1 : -1));
+          campaigns.sort((a, b) => (a.color > b.color ? 1 : -1));
+
           this.accounts_select.options = this.removeDuplicateId(accounts, "id");
           this.campaigns_select.options = this.removeDuplicateId(
             campaigns,
@@ -296,7 +384,6 @@ export default {
       });
     },
     selectAllAccounts() {
-      console.log(this.select_account_has_focus);
       //method to select all accounts
       let selected_option = this.accounts_select.options;
       let multiple_value = this.accounts_select.multiple;
@@ -309,6 +396,9 @@ export default {
             multiple_value.push(option.value.toString());
           }
         });
+        this.fetchAllCampaignsFromSelectedAccounts(
+          this.accounts_select.multiple
+        );
       } else {
         multiple_value.splice(0, multiple_value.length);
       }
@@ -346,13 +436,132 @@ export default {
         this.selectCampaigns = false;
       }
     },
-    getAllCampaignsFromSelectedAccounts() {
-      console.log(this.accounts_select.multiple);
+    onSelectAccountOnFocus(elementClosed) {
+      if (elementClosed == false) {
+        this.fetchAllCampaignsFromSelectedAccounts(
+          this.accounts_select.multiple
+        );
+      }
     },
-    onSelectAccountOnFocus(bool) {
-      console.log(bool);
-      if (bool == false) {
-        console.log("focus out");
+    fetchAllCampaignsFromSelectedAccounts(selectedAccountsId) {
+      insightsRef.once("value").then(snapshot => {
+        let list = [];
+
+        snapshot.forEach(childSnapshot => {
+          //get the random generated id
+          var key = childSnapshot.key;
+
+          //get the data inside object
+          var values = snapshot.child(childSnapshot.key).val();
+
+          //loop to get all the
+          var campaigns = [];
+          this.campaigns_select.options = [];
+          this.campaigns_select.multiple = [];
+          this.selectCampaigns = false;
+
+          //loop to get all the campaigns of all selected accounts
+          selectedAccountsId.forEach(account_id => {
+            values.data.forEach((key, value) => {
+              if (account_id == key.account_id) {
+                campaigns.push({
+                  id: key.campaign_id,
+                  label: key.campaign_name,
+                  value: key.campaign_id,
+                  account_id: key.account_id,
+                  account_name: key.account_name,
+                  selected: false
+                });
+              }
+            });
+
+            this.campaigns_select.options = this.removeDuplicateId(
+              campaigns,
+              "id"
+            );
+          });
+        });
+      });
+    },
+    fetchAllAccountsAndCampaignsMetrics() {
+      //form validation
+      if (this.accounts_select.multiple.length <= 0) {
+        console.log("No Accounts Selected");
+      } else if (this.campaigns_select.multiple.length <= 0) {
+        console.log("No Campaigns Selected");
+      } else if (!this.start_date) {
+        console.log("No Start Date Selected");
+      } else if (!this.end_date) {
+        console.log("No End Date Selected");
+      } else {
+        console.log("Fetching Metrics");
+
+        insightsRef.once("value").then(snapshot => {
+          let list = [];
+
+          snapshot.forEach(childSnapshot => {
+            //get the random generated id
+            var key = childSnapshot.key;
+
+            //get the data inside object
+            var values = snapshot.child(childSnapshot.key).val();
+
+            //loop to get all the
+            var cpc = [];
+            var cpm = [];
+            var cpp = [];
+            var ctr = [];
+            var label = [];
+
+            var metrics = [];
+
+            var date_start = new Date(this.start_date);
+            var date_end = new Date(this.end_date);
+
+            this.campaigns_select.multiple.forEach(campaign_id => {
+              values.data.forEach((key, value) => {
+                if (campaign_id == key.campaign_id) {
+                  var cpc_value = parseFloat(key.cpc);
+                  var cpm_value = parseFloat(key.cpm);
+                  var cpp_value = parseFloat(key.cpp);
+                  var ctr_value = parseFloat(key.ctr);
+
+                  //condition to check whether the date start of key is already in metrics array
+                  if (
+                    metrics.filter(
+                      e => e.date_start == key.date_start.toString()
+                    ).length > 0
+                  ) {
+                    //loop to add the cpp, cpm, cpp, ctr data to metrics array with the same date
+                    for (var i = 0; i < metrics.length - 1; i++) {
+                      if (key.date_start == metrics[i].date_start) {
+                        metrics[i].data.cpc =
+                          cpc_value + parseFloat(metrics[i].data.cpc);
+                        metrics[i].data.cpm =
+                          cpm_value + parseFloat(metrics[i].data.cpm);
+                        metrics[i].data.cpp =
+                          cpp_value + parseFloat(metrics[i].data.cpp);
+                        metrics[i].data.ctr =
+                          ctr_value + parseFloat(metrics[i].data.ctr);
+                      }
+                    }
+                  } else {
+                    var properties = {
+                      date_start: key.date_start.toString(),
+                      data: {
+                        cpc: cpc_value,
+                        cpm: cpm_value,
+                        cpp: cpp_value,
+                        ctr: ctr_value
+                      }
+                    };
+                    metrics.push(properties);
+                  }
+                }
+              });
+            });
+          });
+        });
       }
     }
   },
@@ -409,9 +618,7 @@ export default {
           }
           var date_start = key.date_start.toString();
           var date_stop = key.date_stop.toString();
-          label.push(
-            key.date_start.toString() + " - " + key.date_stop.toString()
-          );
+          label.push(key.date_start.toString());
         });
 
         bigChartData.push(cpc);
